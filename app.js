3f5d63e6e8ed8ba91f49b758b21fbea86babaf81
John Doe <johndoe@example.com>
2019-03-19 15:27:05 +0800

    配制服务器的有效性

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express=require('express');
+const sha1=require('sha1');
 const app=express();
-app.get('/',(req,res)=>{
+app.use((req,res)=>{
+    //获取到微信服务器发送的请求参数
+    const {signature,echostr,timestamp,nonce}=req.query;
     
+    //设置好的token值
+    const token='todayis190319';
+
+    //将三者字典排序
+    const arr=[token,timestamp,nonce].sort();
+
+    //将数据拼接成一个字符串并sha1加密
+    const sha1Str=sha1(arr.join(''));
+
+    判断数据是否来自于微信服务器
+    if(sha1Str===signature){        
+        res.end(echostr);
+    }else{
+        res.end('error');
+    }
 })
 app.listen(3000,(err)=>{
     if(!err) console.log('服务器启动成功');
